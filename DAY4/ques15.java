60986b3a35aa6c27c6a415c391c633301441878d
gaurigupta-7 <gauriii5679@gmail.com>
2026-06-03 17:59:03 +0530

    Fixed code

--- DAY4/ques15.java
+++ DAY4/ques15.java
@@ -4,21 +4,30 @@ public class ques15
     public static void main(String[] args)
     {
         Scanner sc=new Scanner (System.in);
-        System.out.println("enter a number to find the armstrong of:");
+        System.out.println("enter a number to check the armstrong of:");
         int n=sc.nextInt();
         int t=n,d,arm=0,c=0;
         while(t!=0)
         {
             d=t%10;
             c++;
+            t=t/10;
         }
         t=n;
         while(t!=0)
         {
             d=t%10;
             arm=arm+Math.pow(d,c);
+            t=t/10;
 
         }
-        System.out.println("Armstrong number of"+n+"="+arm);
+        if (n==arm)
+        {
+            System.out.println(n+" is an armstrong number");
+        }
+        else
+        {
+            System.out.println(n+" is not an armstrong number");
+        }
     }
 }
