835ddf8fbc3449cd30d71d3f4f0c5d2c4c533ed8
gaurigupta-7 <gauriii5679@gmail.com>
2026-06-03 17:03:36 +0530

    fi

--- DAY2/ques7.java
+++ DAY2/ques7.java
@@ -6,13 +6,13 @@ public class ques7
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number");
         int n=sc.nextInt();
-        int d,prod=1;
+        int d,prod=1, t=n;
         while (n!=0)
         {
             d=n%10;
             prod=prod*d;
-
+            n=n/10;
         }
-        System.out.println("Product of digits of a number"+n+"="+prod);
+        System.out.println("Product of digits of a number "+t+" = "+prod);
     }
 }
