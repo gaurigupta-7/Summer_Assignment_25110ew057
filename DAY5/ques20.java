a254bd9afb54b9d8e3589eef5d649f0bb77608b3
gaurigupta-7 <gauriii5679@gmail.com>
2026-06-03 21:36:53 +0530

    Add functionality to find largest prime factor

--- DAY5/ques20.java
+++ DAY5/ques20.java
@@ -1,5 +1,31 @@
 package DAY5;
-
-public class ques20 {
-    
+import java.util.*;
+public class ques20
+{
+     public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to find prime factors of");
+        int n=sc.nextInt();
+        int i,j,c,pfact=0;
+        for(i=1;i<n;i++)
+            {
+                if(n%i==0)
+                {
+                    c=0;
+                    for(j=1;j<i;j++)
+                        {
+                            if(i%j==0)
+                            {
+                                c++;
+                            }
+                        }
+                    if(c==1)
+                    {
+                        pfact=i;
+                    }
+                }
+            }
+        System.out.println("The largest prime factor of "+n+" is "+pfact);
+    }
 }
