5de33ece659c9bcfb034d880ccaa1b62ebac4ebd
gaurigupta-7 <gauriii5679@gmail.com>
2026-06-03 17:13:09 +0530

    Fixed code

--- DAY3/ques10.java
+++ DAY3/ques10.java
@@ -4,7 +4,7 @@ public class ques10
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        Syste.out.println("Enter starting of range");
+        System.out.println("Enter starting of range");
         int rs=sc.nextInt();
         System.out.println("Enter ending of range");
         int re=sc.nextInt();
